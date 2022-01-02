82e880a672f3b527eaad397b9eacab7dea2b86c8
Patrick Coady <patrickscoady42@gmail.com>
2022-01-02 20:55:57 +0000

    filled in lots of detials

--- RasPiNode.py
+++ RasPiNode.py
@@ -1,3 +1,4 @@
+#sudo python /home/pi/Documents/habit/RasPiNode.py &
 import requests
 from gpiozero import Button
 import time
@@ -6,12 +7,15 @@ from gpiozero import LED
 
 
 url = 'http://192.168.86.25:5000//habit'
-actions = {"deorder":{"button": Button(21), "light": LED(19)}}
+actions = {"run":{"button": Button(15), "light": LED(16)},
+           #"yoga":{"button": Button(), "light": LED(19)},
+           "weight":{"button": Button(18), "light": LED(14)},
+           "dog":{"button": Button(17), "light": LED(21)}}
 
 
-bad_request = LED(20)
-no_response = LED(16)
-success = LED(26)
+bad_request = LED(26)
+no_response = LED(1)
+success = LED(20)
 
 
 #led = LED(4)
@@ -27,9 +31,11 @@ def increment_habit(habitName, date, action):
         print("here")
         return "404"
     if response.json()["isError"]:
+        print("here1")
         return "dup"
     else:
         actions[action]["light"].on()
+        print("here2")
         return "200"
     
 
@@ -37,7 +43,7 @@ while True:
     response = None
     for action in actions.keys():
         if actions[action]["button"].is_pressed:
-            response = increment_habit("test", "2021-12-02", action)
+            response = increment_habit("test", "2021-12-05", action)
     if response == "200":
         success.on()
         time.sleep(1)
